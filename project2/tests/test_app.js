2969cdb8b1b20199f5e1c5d27108a2b30ca1e7b2
inesaaaa <Inesa.Afitsaryan@am-unit-0108.yes.labs.intellij.net>
2025-01-20 13:13:57 +0400

    Remove project2 directory

--- project2/tests/test_app.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@jest/globals');
-const greet = (name) => `Hello, ${name}!`;
-
-test('greet function', () => {
-    expect(greet('World')).toBe('Hello, World!');
-});
